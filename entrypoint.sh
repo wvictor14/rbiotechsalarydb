22e133474365737b78ab82e3ed4648c8573cfeaf
Victor Yuan <victor.2wy@gmail.com>
2025-10-26 15:14:36 -0700

    move initial populate to beginning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e # exit on error
 
+# Run initial ETL
+cd /app && uv run --frozen csv_to_db.py
+
 # writes the environment variables to file, for crontab access
 # https://stackoverflow.com/questions/27771781/how-can-i-access-docker-set-environment-variables-from-a-cron-job
 printenv | grep -v "no_proxy" >> /etc/environment
 
-# Start cron in background
+# Keep container running and follow logs
 service cron start
 
-# Run initial ETL
-cd /app && uv run --frozen csv_to_db.py
-
-# Keep container running and follow logs
+# Follow logs (this keeps container running and shows output)
 exec tail -f /var/log/cron.log
